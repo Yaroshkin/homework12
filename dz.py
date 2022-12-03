99a5f9f1af3de1d3895b34ba6e086cbaf19bdc9a
yaroshkin <yaroshkin255@ukr.net>
2022-12-03 20:28:36 +0200

    change

--- dz.py
+++ dz.py
@@ -1,31 +1,31 @@
 
 # 1.Поиск и замена слов в содержимом текстового файла
-# def search_file(file, word_find, word_replace):
-#     with open(file, mode="r") as f:
-#         li1 = f.readline()
-#     with open(file, mode="w") as f:
-#         li1 = li1.replace(word_find,word_replace)
-#         f.writelines(li1)
-# print(search_file("hw12.txt","4","stope"))
+def search_file(file, word_find, word_replace):
+    with open(file, mode="r") as f:
+        li1 = f.readline()
+    with open(file, mode="w") as f:
+        li1 = li1.replace(word_find,word_replace)
+        f.writelines(li1)
+print(search_file("hw12.txt","4","stope"))
 
 # 2. Подсчет количества слов в содержимом текстового файла
 
-# def score(file):
-#     with open(file, mode="r") as f:
-#         li1 = f.readline()
-#         li1 = li1.split(" ")
-#     print(len(li1))
-# score("hw12.txt")
+def score(file):
+    with open(file, mode="r") as f:
+        li1 = f.readline()
+        li1 = li1.split(" ")
+    print(len(li1))
+score("hw12.txt")
 
 #3. Вывести слова содержимого файла  в обратном порядке
 
-# def revers(file):
-#     with open(file, mode="r") as f:
-#         li1 = f.readline()
-#         li1 = li1.split(",")
-#         li1 = " ".join(reversed(li1))
-#         print(li1)
-# revers("hw12.txt")
+def revers(file):
+    with open(file, mode="r") as f:
+        li1 = f.readline()
+        li1 = li1.split(",")
+        li1 = " ".join(reversed(li1))
+        print(li1)
+revers("hw12.txt")
 
 # 4. Удаление заданной  по номеру строки из файла
 def delate(file):
